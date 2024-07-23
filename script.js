c6d76f575b150aadca04641a49035f101567e1b6
Sudhanshu Shekhar Jha <156537716+sudhanshu-j@users.noreply.github.com>
2024-07-24 00:06:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+// Selecting DOM elements
+
+const captchaTextBox = document.querySelector(".captcha_box input");
+const refreshButton = document.querySelector(".refresh_button");
+const captchaInputBox = document.querySelector(".captcha_input input");
+const message = document.querySelector(".message");
+const submitButton = document.querySelector(".button");
+
+// Storing generated captcha
+
+let captchaText = null;
+
+//creating  function for generating the captcha
+
+const generateCaptcha = () => {
+  const randomString = Math.random().toString(36).substring(2, 7);
+  const randomStringArray = randomString.split("");
+  const changeString = randomStringArray.map((char) =>
+    Math.random() > 0.5 ? char.toUpperCase() : char
+  );
+  captchaText = changeString.join("   ");
+  captchaTextBox.value = captchaText;
+  console.log(captchaText);
+};
+
+const refreshBtnClick = () => {
+  generateCaptcha();
+  captchaInputBox.value = "";
+  captchaKeyUpValidate();
+};
+
+const captchaKeyUpValidate = () => {
+  // Disabling the submit button
+
+  submitButton.classList.toggle("disabled", !captchaInputBox.value);
+
+  if (!captchaInputBox.value) message.classList.remove("active");
+};
+
+// Function to validate the entered captcha
+
+const submitBtnClick = () => {
+  captchaText = captchaText
+    .split("")
+    .filter((char) => char !== " ")
+    .join("");
+  message.classList.add("active");
+
+  // checking the entered captcha text is correct or not
+
+  if (captchaInputBox.value === captchaText) {
+    message.innerText = "Entered captcha is correct";
+    message.style.color = "#159752";
+  } else {
+    message.innerText = "Entered captcha is incorrect";
+    message.style.color = "#FF2525";
+  }
+};
+
+// Event listener for....
+
+refreshButton.addEventListener("click", refreshBtnClick);
+captchaInputBox.addEventListener("keyup", captchaKeyUpValidate);
+submitButton.addEventListener("click", submitBtnClick);
+
+generateCaptcha();
